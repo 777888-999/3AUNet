b30c64c809010eb68be0d63a45da10563e14ac28
777888-999 <69074029+777888-999@users.noreply.github.com>
2020-08-02 14:44:20 +0800

    Add files via upload

---
+++ Constants.py
@@ -0,0 +1,13 @@
+import os
+import shutil
+
+Image_size = (448, 448)
+#ROOT = './dataset/DRIVE'
+ROOT = './dataset/CHASEDB'
+BATCHSIZE_PER_CARD = 2
+TOTAL_EPOCH = 200
+INITAL_EPOCH_LOSS = 10000
+NUM_EARLY_STOP = 20
+NUM_UPDATE_LR = 10
+
+BINARY_CLASS = 1
